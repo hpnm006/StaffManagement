c33196a740532e00e40df786ad86e49373411b85
DoHoaiThu <dohoaithu3103@gmail.com>
2026-06-26 23:53:16 +0700

    Add files via upload

---
+++ src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Role (Role_ID, Role_Name) VALUES (1, 'MANAGER');
+INSERT INTO Role (Role_ID, Role_Name) VALUES (2, 'STAFF');
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Role (
+    Role_ID INT PRIMARY KEY,
+    Role_Name VARCHAR(50)
+);
+
+CREATE TABLE Staff (
+    StaffID INT AUTO_INCREMENT PRIMARY KEY,
+    FullName VARCHAR(100),
+    Gender BIT,
+    PhoneNumber VARCHAR(20),
+    Email VARCHAR(100),
+    Password VARCHAR(100),
+    Role_ID INT,
+    IsActive BIT,
+    StaffCode VARCHAR(20),
+    DateOfBirth DATE,
+    Department VARCHAR(100),
+    Position VARCHAR(100),
+    Salary DOUBLE,
+    HireDate DATE,
+    FOREIGN KEY (Role_ID) REFERENCES Role(Role_ID)
+);
